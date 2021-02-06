cc3d66ca525ee722871204487b6891773d53eb39
javan9916 <javaraya9916@gmail.com>
2021-02-05 19:28:19 -0600

    preparing content

--- webapp/views.py
+++ webapp/views.py
@@ -1,34 +1,37 @@
 from django.shortcuts import render
 
-####### global variables #########
-style = 'styles/styles.css'     ##
-##################################
+####### global variables #################
+styles_home = 'styles/home.css'         ##
+styles_projects = 'styles/projects.css' ##
+styles_plans = 'styles/plans.css'       ##
+styles_about = 'styles/about.css'       ##
+##########################################
 
 def home(request):
     context = {
         'home_page': 'active',
-        'styles': style,
+        'styles': styles_home,
     }
     return render(request, 'home.html', context)
 
 def projects(request):
     context = {
         'project_page': 'active',
-        'styles': style
+        'styles': styles_projects
     }
     return render(request, 'projects.html', context)
     
 def plans(request):
     context = {
         'plan_page': 'active',
-        'styles': style
+        'styles': styles_plans
     }
     return render(request, 'plans.html', context)
         
 def about(request):
     context = {
         'about_page': 'active',
-        'styles': style
+        'styles': styles_about
     }
     return render(request, 'about.html', context)
     
